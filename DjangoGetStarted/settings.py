c8e6bee93a884f93928f6fbe82aca6f5335d5b96
Xinjie <2314388662@qq.com>
2018-06-15 09:06:16 +0800

    getstart over

--- DjangoGetStarted/settings.py
+++ DjangoGetStarted/settings.py
@@ -78,11 +78,19 @@ WSGI_APPLICATION = 'DjangoGetStarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdjango',
+        'USER': 'root',
+        'PASSWORD': 'xinjie',
+        'HOST': "127.0.0.1"
     }
 }
 
+
+STATIC_URL = '/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
--- apps/message/views.py
+++ apps/message/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
+def getfrom(request):
+    return render(request, "form.html")
+
+
 # Create your views here.
