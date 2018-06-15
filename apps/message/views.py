c06bf4039a37f07c0d5edf55781f64f20e838017
Xinjie <2314388662@qq.com>
2018-06-15 09:56:31 +0800

    getstart overed

--- apps/message/views.py
+++ apps/message/views.py
@@ -3,25 +3,28 @@ from message.models import UserMessage
 
 
 def getfrom(request):
-    all_message = UserMessage.objects.all()
-    for message in all_message:
-        print(message.name)
+    message = None
+    all_message = UserMessage.objects.filter(name="xinjie")
 
-    if  request.method == "POST":
-        name=request.POST.get("name", '')
-        message = request.POST.get("message", '')
-        address = request.POST.get("address", '')
-        email = request.POST.get("email", '')
+    if all_message:
+        message = all_message[0]
 
-        user_message = UserMessage()
-        user_message.name = name
-        user_message.message = message
-        user_message.address = address
-        user_message.email = email
-        user_message.object_id = "agadc"
-        user_message.save()
+    # if  request.method == "POST":
+    #     name=request.POST.get("name", '')
+    #     message = request.POST.get("message", '')
+    #     address = request.POST.get("address", '')
+    #     email = request.POST.get("email", '')
+    #
+    #     user_message = UserMessage()
+    #     user_message.name = name
+    #     user_message.message = message
+    #     user_message.address = address
+    #     user_message.email = email
+    #     user_message.object_id = "agadc"
+    #     user_message.save()
 
-    return render(request, "form.html")
+    return render(request, "form.html",
+                  {"my_message": message})
 
 
 # Create your views here.
