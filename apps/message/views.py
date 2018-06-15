57afbcda6f1338620a86202b61f27ef8d35e39c3
Xinjie <2314388662@qq.com>
2018-06-15 09:40:20 +0800

    getstart over3

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from message.models import UserMessage
 
 
 def getfrom(request):
+    all_message = UserMessage.objects.all()
+    for message in all_message:
+        print(message.name)
+
+    if  request.method == "POST":
+        name=request.POST.get("name", '')
+        message = request.POST.get("message", '')
+        address = request.POST.get("address", '')
+        email = request.POST.get("email", '')
+
+        user_message = UserMessage()
+        user_message.name = name
+        user_message.message = message
+        user_message.address = address
+        user_message.email = email
+        user_message.object_id = "agadc"
+        user_message.save()
+
     return render(request, "form.html")
 
 
